bb9fccaf0252fa3bbf1430f027d1d9dd48bcac34
U-PS14B22\vantan <akira206@gmail.com>
2015-06-11 20:14:04 +0900

    c-sharp enum

---
+++ enum/enum/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+enum SceneType { 
+  Title,
+  GameMain,
+  Result
+}
+
+class Program {
+  static void Main(string[] args) {
+    SceneType scene = SceneType.GameMain;
+
+    switch (scene) { 
+      case SceneType.Title:
+        Console.WriteLine(scene);
+        Console.WriteLine((int)scene);
+      break;
+
+      case SceneType.GameMain:
+        Console.WriteLine(scene);
+        Console.WriteLine((int)scene);
+      break;
+
+      case SceneType.Result:
+        Console.WriteLine(scene);
+        Console.WriteLine((int)scene);
+      break;
+    }
+  }
+}
