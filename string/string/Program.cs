107944c5b621b37d5d84cacc1b7aa3b9ace40faa
U-PS14B22\vantan <akira206@gmail.com>
2015-06-15 16:36:23 +0900

    string追加[C++とさほど変わりはない]

---
+++ string/string/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @string {
+  class Program {
+    static void Main(string[] args) {
+      //---------------------------------------------------
+      // char
+      char char_test = 'a';
+      Console.WriteLine(char_test);
+      char_test = 'b';
+      Console.WriteLine(char_test);
+
+      char[] n = {'a', 'b', 'c', 'd', 'e'};
+      Console.WriteLine(n); // abcde
+      n[4] = 'a';
+      Console.WriteLine(n); // abcda
+      // n[5] = 'a'; // err
+      Console.WriteLine(n[1]); // b
+
+
+      //---------------------------------------------------
+      // string
+      string name = "yanai";
+      Console.WriteLine(name); // yanai
+      Console.WriteLine(name[0]); // y
+    }
+  }
+}
