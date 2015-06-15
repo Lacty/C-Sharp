56e08f8cc637a71fc0d1e42b078146e94ca274c5
U-PS14B22\vantan <akira206@gmail.com>
2015-06-15 16:19:17 +0900

    array追加[C++とさほど変わりはない]

---
+++ array/array/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace array {
+  class Program {
+    static void Main(string[] args) {
+      int[] val = { 0, 1, 2, 3, 4 };
+      for (int i = 0; i < 5; ++i) {
+        Console.WriteLine("val[{0}] = {1}", i, val[i]);
+      }
+    }
+  }
+}
