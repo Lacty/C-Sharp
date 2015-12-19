35804baad79544bb496042e6853613ced6b2d144
Lacty <akira206@gmail.com>
2015-12-19 16:22:18 +0900

    array更新

--- array/array.cs
+++ array/array.cs
@@ -4,8 +4,8 @@ using System;
 class ArrayTest {
   static void Main(string[] args) {
     int[] val = { 0, 1, 2 };
-    Console.WriteLine("val[{0}] = {1}", 0, val[0]);
-    Console.WriteLine("val[{1}] = {1}", 1, val[1]);
-    Console.WriteLine("val[{1}] = {1}", 2, val[2]);
+    for (int i = 0; i < 3; i++) {
+      Console.WriteLine("val[{0}] = {1}", i, val[i]);
+    }
   }
 }
